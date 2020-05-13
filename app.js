cfcab0aebc69387a1b3ba3d3d3292540a17c9bd8
DevAdel <54174192+DevAdel@users.noreply.github.com>
2020-05-13 23:32:49 +0100

    Increase security using bcrypt hashing

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -36,23 +37,34 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  // Create a new user
-  // The user email is going to be whatever the user inputs in the email field
-  // The password is going to be whatever the user inputs in the password field
-  const user = new User({
-    email: req.body.username,
-    password: md5(req.body.password),
-  });
+  /* Parameters
+  1) What you want to hash
+  2) Salt Rounds
+  3) Callback function
+    Parameters
+    1) error
+    2) The hashed password
+  */
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    // Create a new user
+    // The user email is going to be whatever the user inputs in the email field
+    // The password is going to be whatever the user inputs in the password field
+    const user = new User({
+      email: req.body.username,
+      // Set the password to the hashed version of what the user inputed in the password field
+      password: hash,
+    });
 
-  // Save the user to the database
-  user.save((err) => {
-    err ? console.log(err) : res.render("secrets");
+    // Save the user to the database
+    user.save((err) => {
+      err ? console.log(err) : res.render("secrets");
+    });
   });
 });
 
 app.post("/login", (req, res) => {
   const email = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   // Find a user that has an email of whatever the user has inputed in the email field
   // email #1 = the email of the user in the database
@@ -61,9 +73,15 @@ app.post("/login", (req, res) => {
     if (err) {
       console.log(err);
     } else {
-      // If this user exists and the password of the found user is equal to what the user inputed in the password field
-      if (foundUser && foundUser.password === password) {
-        res.render("secrets");
+      // If this user exists
+      if (foundUser) {
+        // If the password that the user has inputed (hashed version) is equal to the hashed password stored in the database
+        // result will either be true, if the password (hashed) is equal to the hashed password which is stored in the database, and will be false if the password (hashed) is not equal to the hashed password stored in the database
+        bcrypt.compare(password, foundUser.password, (err, result) => {
+          if (result) {
+            res.render("secrets");
+          }
+        });
       }
     }
   });
